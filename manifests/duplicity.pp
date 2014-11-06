f0b80618b1597455e6e163e959ff9716414c1169
kirby@puppetlabs.com <kirby@puppetlabs.com>
2014-11-06 15:58:22 -0800

    Adding ability to automatically load AWS keys without leaving them set as environment variables

--- manifests/duplicity.pp
+++ manifests/duplicity.pp
@@ -3,17 +3,21 @@ define zpr::duplicity (
   $target,
   $home,
   $key_id,
-  $ensure        = present,
-  $keep          = '8W',
-  $hour          = '1',
-  $minute        = '10',
-  $monthday_inc  = [ '2', '3' ],#[ range('2', '15'), range('17', '31') ],
-  $monthday_full = [ '1', '16' ],
-  $options       = undef
+  $aws_key_file,
+  $ensure         = present,
+  $keep           = '8W',
+  $hour           = '1',
+  $minute         = '10',
+  $monthday_inc   = [ "range('2', '15')", "range('17', '31')" ],
+  $monthday_full  = [ '1', '16' ],
+  $monthday_clean = '*',
+  $task_spooler   = '/usr/bin/tsp',
+  $options        = undef
 ) {
 
   include duplicity::install
   include zpr::resource::gpg
+  include zpr::resource::task_spooler
 
   # Set variables
 
@@ -29,41 +33,46 @@ define zpr::duplicity (
   # Assemble commands
 
   if $options {
-    $cmd_options = inline_template("${duplicity} <%= options.join(' ') %>")
+    $cmd_prefix = inline_template("${duplicity} <%= options.join(' ') %>")
   }
   else {
-    $cmd_options = inline_template("${duplicity} <%= default_options.join(' ') %>")
+    $cmd_prefix = inline_template("${duplicity} <%= default_options.join(' ') %>")
   }
-  $cmd_suffix  = "${name} ${target}"
+  $cmd_suffix   = "${name} ${target}"
 
-  $environment_command = "source ${gpg_agent_info}; export GPG_AGENT_INFO;"
+  $environment_command = "source ${gpg_agent_info}; source ${aws_key_file}; export GPG_AGENT_INFO;"
 
-  $incr_cmd = "/bin/bash -c '${environment_command} ${cmd_prefix} incremental ${cmd_suffix}' && echo `date` > ${lastrun}"
-  $full_cmd = "/bin/bash -c '${environment_command} ${cmd_prefix} full ${cmd_suffix}' && echo `date` > ${lastrun}"
+  $full        = "full ${cmd_suffix}' && echo `date` > ${lastrun}"
+  $incremental = "incremental ${cmd_suffix}' && echo `date` > ${lastrun}"
+  $clean       = "remove-older-than ${keep} --force ${target}"
+  $base        = "${task_spooler} /bin/bash -c"
 
-  $clean_cmd = "/bin/bash -c '${environment_command} ${cmd_prefix} remove-older-than ${keep} --force ${target}'"
+  $full_cmd    = "${base} '${environment_command} ${cmd_prefix} ${full}"
+  $incr_cmd    = "${base} '${environment_command} ${cmd_prefix} ${incremental}"
+  $clean_cmd   = "${base} '${environment_command} ${cmd_prefix} ${clean}'"
 
   Cron {
     ensure      => $ensure,
-    user        => $user,
+    user        => $user
   }
 
   cron {
   # Run nightly incrementals
     "Duplicity: incremental backup of ${title}":
-      command     => $incr_cmd,
-      hour        => $hour,
-      minute      => $minute,
-      monthday    => $monthday_inc;
+      command  => $incr_cmd,
+      hour     => $hour,
+      minute   => $minute,
+      monthday => $monthday_inc;
   # Run full backups once every two weeks or as configured
     "Duplicity: full backup of ${title}":
-      command     => $full_cmd,
-      hour        => $hour,
-      minute      => $minute,
-      monthday    => $monthday_full;
+      command  => $full_cmd,
+      hour     => $hour,
+      minute   => $minute,
+      monthday => $monthday_full;
   # Remove old backups
     "Duplicity: remove old ${title} backups":
-      command     => $clean_cmd,
-      hour        => $hour;
+      command  => $clean_cmd,
+      hour     => $hour,
+      monthday => $monthday_clean
   }
 }
---
+++ manifests/resource/aws.pp
@@ -0,0 +1,18 @@
+class zpr::resource::aws (
+) {
+
+  include zpr::params
+
+  $home           = $zpr::params::home
+  $user           = $zpr::params::user
+  $aws_key_file   = $zpr::params::aws_key_file
+  $aws_access_key = $zpr::params::aws_access_key
+  $aws_secret_key = $zpr::params::aws_secret_key
+
+  file { "${home}/${aws_key_file}":
+    ensure  => file,
+    owner   => $user,
+    mode    => '0400',
+    content => template('zpr/resource/aws.erb')
+  }
+}
