e8de44c930497c0566cd661fd363631c8d45bf6b
kirby@puppetlabs.com <kirby@puppetlabs.com>
2014-11-11 11:43:39 -0800

    Adding homedir to path for gpg agent info

--- manifests/resource/gpg.pp
+++ manifests/resource/gpg.pp
@@ -3,14 +3,15 @@ class zpr::resource::gpg {
   include zpr::params
 
   $user           = $zpr::params::user
+  $home           = $zpr::params::home
   $gpg_passphrase = $zpr::params::gpg_passphrase
   $gpg_key_grip   = $zpr::params::gpg_key_grip
-
 
   gpg::agent { $user:
     gpg_passphrase => $gpg_passphrase,
     gpg_key_grip   => $gpg_key_grip,
     user           => $user,
+    outfile        => "${home}/.gpg-agent-info",
     options        => [
       '--default-cache-ttl 999999999',
       '--max-cache-ttl     999999999',
