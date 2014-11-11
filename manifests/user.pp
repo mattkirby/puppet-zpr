d5fb6472dc8f9dcb8954dc9a7bcb6dfa4c3b43f3
kirby@puppetlabs.com <kirby@puppetlabs.com>
2014-11-10 16:04:32 -0800

    Remove requirement for user when exporting ssh key

--- manifests/user.pp
+++ manifests/user.pp
@@ -50,7 +50,7 @@ class zpr::user (
       type    => 'ssh-rsa',
       user    => $user,
       tag     => $::hostname,
-      require => User[$user]
+      #require => User[$user]
     }
 
     Ssh_authorized_key <<| tag == $tag |>>
