d1b342b006947baed0d3ee8ecf1bf9140c88f009
kirby@puppetlabs.com <kirby@puppetlabs.com>
2015-01-26 10:27:33 -0800

    Move tags into an array
    Without this change there are unnecessary if statements used to declare
    tags to be checked for collection in worker, storage and offsite
    classes. Additionally, the ssh key for the worker user is not collected
    unless the environment matches $env_tag. This commit moves tags into an array so that it may be
    referenced as $tags and allow removal of the if statements. The
    ssh_authorized_key resource for the zpr worker user is now collected
    regardless of environment when a backup job is declared.

--- manifests/worker.pp
+++ manifests/worker.pp
@@ -4,22 +4,13 @@ class zpr::worker (
   $env_tag    = $zpr::params::env_tag,
 ) inherits zpr::params {
 
+  $tags = [ $worker_tag, $env_tag ]
+
   include zpr::user
   include zpr::resource::backup_dir
 
-  if $env_tag {
-    File       <<| tag == $env_tag and tag == $worker_tag |>>
-    Mount      <<| tag == $env_tag and tag == $worker_tag |>> {
-      options => 'rw'
-    }
-    Zpr::Rsync <<| tag == $env_tag and tag == $worker_tag |>>
-  }
-  else {
-    File       <<| tag == $worker_tag |>>
-    Mount      <<| tag == $worker_tag |>> {
-      options => 'rw'
-    }
-    Zpr::Rsync <<| tag == $worker_tag |>>
-  }
+  File       <<| tag == $tags |>>
+  Mount      <<| tag == $tags |>> { options => 'rw' }
+  Zpr::Rsync <<| tag == $tags |>>
 
 }
