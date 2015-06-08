ae2e9045db959347e0b27cdeaae8faf9cdf9534d
kirby@puppetlabs.com <mk.kirby@gmail.com>
2015-06-08 16:18:03 -0700

    Revert "Reformat base command for duplicity"
    This reverts commit 022698928cbf8115bdab345c487f8c60d36def15.

--- manifests/duplicity.pp
+++ manifests/duplicity.pp
@@ -64,18 +64,11 @@ define zpr::duplicity (
 
   $full        = "--full-if-older-than ${full_every} ${cmd_suffix}"
   $clean       = "remove-older-than ${keep} --force ${target}"
-  $source_var  = "/bin/bash -c \"source /var/lib/zpr/.tsprc ;"
   $tsp         = "${task_spooler} /bin/bash -c"
-  $base        = [
-    $source_var,
-    $tsp,
-    "'",
-    $environment_command,
-    $cmd_prefix
-  ]
+  $base        = [ $tsp, "'", $environment_command, $cmd_prefix ]
 
-  $full_cmd    = join( [ $base, $full, "'\"" ], ' ')
-  $clean_cmd   = join( [ $base, $clean, "'\"" ], ' ')
+  $full_cmd    = join( [ $base, $full, "'" ], ' ')
+  $clean_cmd   = join( [ $base, $clean, "'"], ' ')
 
   cron {
   # Run full backups as configured witht incremental in beetween
