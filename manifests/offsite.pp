2d640c430f98d0e07ba8008e29995029157fdcd9
kirby@puppetlabs.com <mk.kirby@gmail.com>
2015-11-24 13:48:37 -0800

    Include gpg in offsite role
    This commit includes the zpr::gpg class in the zpr::offsite class.
    Without this change the gpg::agent is not deployed on offsite nodes.

--- manifests/offsite.pp
+++ manifests/offsite.pp
@@ -7,6 +7,7 @@ class zpr::offsite (
   include zpr::backup_dir
   include zpr::task_spooler
   include zpr::lockfile_progs
+  include zpr::gpg
 
   File  <<| tag == $readonly_tag and tag == 'zpr_vol' |>>
   Mount <<| tag == $readonly_tag and tag == 'zpr_vol' |>> {
